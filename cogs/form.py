ecaf8e8d02d78a56ab1b6d25dd4067fa7a9d26fe
mackenzie <mackenzie@yourmoms>
2023-06-19 14:43:08 -0500

    feat: block non-author interaction with UI

--- cogs/form.py
+++ cogs/form.py
@@ -30,57 +30,59 @@ async def get_form(ctx, options: dict[list[Field]], display_only=False):
     class ButtonView(discord.ui.View):
         @discord.ui.button(label="EDIT", style=discord.ButtonStyle.primary, row=2)
         async def edit_button_callback(self, button, interaction):
-            fields = options[list(options.keys())[paginator.current_page]]
+            if interaction.user.id == ctx.author.id:
+                fields = options[list(options.keys())[paginator.current_page]]
 
-            class EditModal(discord.ui.Modal):
-                def __init__(self, *args, **kwargs) -> None:
-                    super().__init__(*args, **kwargs)
-                    for field in fields:
-                        self.add_item(
-                            discord.ui.InputText(
-                                label=field.label, value=str(field.value)
+                class EditModal(discord.ui.Modal):
+                    def __init__(self, *args, **kwargs) -> None:
+                        super().__init__(*args, **kwargs)
+                        for field in fields:
+                            self.add_item(
+                                discord.ui.InputText(
+                                    label=field.label, value=str(field.value)
+                                )
                             )
-                        )
 
-                async def callback(self, interaction: discord.Interaction):
-                    current_page = paginator.current_page
-                    for i in range(len(fields)):
-                        fields[i].value = self.children[i].value
-                        if fields[i].is_valid():
-                            page_list[current_page].fields[i].value = str(
-                                self.children[i].value
-                            )
-                        else:
-                            page_list[current_page].fields[i].value = (
-                                str(self.children[i].value)
-                                + " "
-                                + error_text("invalid")
-                            )
-                    await interaction.response.edit_message(content="Success")
-                    await paginator.goto_page(current_page)
+                    async def callback(self, interaction: discord.Interaction):
+                        current_page = paginator.current_page
+                        for i in range(len(fields)):
+                            fields[i].value = self.children[i].value
+                            if fields[i].is_valid():
+                                page_list[current_page].fields[i].value = str(
+                                    self.children[i].value
+                                )
+                            else:
+                                page_list[current_page].fields[i].value = (
+                                    str(self.children[i].value)
+                                    + " "
+                                    + error_text("invalid")
+                                )
+                        await interaction.response.edit_message(content="Success")
+                        await paginator.goto_page(current_page)
 
-            modal = EditModal(title=list(options.keys())[paginator.current_page])
-            await interaction.response.send_modal(modal)
+                modal = EditModal(title=list(options.keys())[paginator.current_page])
+                await interaction.response.send_modal(modal)
 
         @discord.ui.button(label="SUBMIT", style=discord.ButtonStyle.green, row=2)
         async def submit_button_callback(self, button, interaction):
-            valid = True
-            for key in list(options.keys()):
-                for field in options[key]:
-                    if not field.is_valid():
-                        valid = False
-                        break
+            if interaction.user.id == ctx.author.id:
+                valid = True
+                for key in list(options.keys()):
+                    for field in options[key]:
+                        if not field.is_valid():
+                            valid = False
+                            break
 
-            if valid:
-                response.append(options)
-                paginator.edit("Success", delete_after=0)
-                await interaction.response.edit_message(
-                    content="Success", view=None, embed=None, delete_after=0
-                )
-            else:
-                await interaction.response.edit_message(
-                    content=error_text("Content contains invalid responses!")
-                )
+                if valid:
+                    response.append(options)
+                    paginator.edit("Success", delete_after=0)
+                    await interaction.response.edit_message(
+                        content="Success", view=None, embed=None, delete_after=0
+                    )
+                else:
+                    await interaction.response.edit_message(
+                        content=error_text("Content contains invalid responses!")
+                    )
 
     paginator = pages.Paginator(
         pages=page_list, custom_view=None if display_only else ButtonView()
--- utils.py
+++ utils.py
@@ -32,9 +32,12 @@ async def get_text_input(ctx, title: str, labels: str | list[str]):
                     self.add_item(discord.ui.InputText(label=label))
 
         async def callback(self, interaction: discord.Interaction):
-            for child in self.children:
-                response.append(child.value)
-            await interaction.response.edit_message(content="Great!", delete_after=0)
+            if interaction.user.id == ctx.author.id:
+                for child in self.children:
+                    response.append(child.value)
+                await interaction.response.edit_message(
+                    content="Great!", delete_after=0
+                )
 
     modal = TextModal(title=title)
     await ctx.send_modal(modal)
@@ -52,11 +55,12 @@ async def get_user_selector_input(ctx, message: str):
     class UserSelector(discord.ui.View):
         @discord.ui.select(select_type=discord.ComponentType.user_select)
         async def select_callback(self, select, interaction):
-            response.append(select.values[0])
-            select.disabled = True
-            await interaction.response.edit_message(
-                content=f"{select.values[0]} selected!", view=None, delete_after=0
-            )
+            if interaction.user.id == ctx.author.id:
+                response.append(select.values[0])
+                select.disabled = True
+                await interaction.response.edit_message(
+                    content=f"{select.values[0]} selected!", view=None, delete_after=0
+                )
 
     await ctx.respond(
         message,
@@ -85,15 +89,41 @@ async def get_selector_input(ctx, message: str, options):
         async def select_callback(
             self, select, interaction
         ):  # the function called when the user is done selecting options
-            response.append(options[int(select.values[0])])
-            select.disabled = True
-            await interaction.response.edit_message(
-                content=f"{options[int(select.values[0])].name} selected!",
-                view=None,
-                delete_after=0,
-            )
+            if interaction.user.id == ctx.author.id:
+                response.append(options[int(select.values[0])])
+                select.disabled = True
+                await interaction.response.edit_message(
+                    content=f"{options[int(select.values[0])].name} selected!",
+                    view=None,
+                    delete_after=0,
+                )
 
     await ctx.respond(message, view=SelectorView())
     while len(response) == 0:
         await asyncio.sleep(1)
     return response[0]
+
+
+async def confirmation_button(ctx, message, confirm_method, cancel_method):
+    class ButtonView(discord.ui.View):
+        @discord.ui.button(label="CANCEL", style=discord.ButtonStyle.red, row=2)
+        async def submit_button_callback(self, button, interaction):
+            if interaction.user.id == ctx.author.id:
+                await cancel_method()
+                await interaction.response.edit_message(
+                    content=f"Success!",
+                    view=None,
+                    delete_after=0,
+                )
+
+        @discord.ui.button(label="OKAY", style=discord.ButtonStyle.green, row=2)
+        async def submit_button_callback(self, button, interaction):
+            if interaction.user.id == ctx.author.id:
+                await confirm_method()
+                await interaction.response.edit_message(
+                    content=f"Success!",
+                    view=None,
+                    delete_after=0,
+                )
+
+    ctx.respond(message, view=ButtonView())
