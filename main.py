19b8a50d675b87af90390cff2f2aeac1a6513f3a
MacKenzie H&C <mackenziehnc@gmail.com>
2024-09-29 11:17:01 -0500

    feat: one mo gen

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-import discord
 import os  # default module
+import discord
 from dotenv import load_dotenv
-from cogs.char import CharCog
-from cogs.game import GameCog
-from cogs.costs import SpendCog
-from cogs.session import SessionCog
-
-load_dotenv()  # load all the variables from the env file
-bot = discord.Bot()
-
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user} is ready and online!")
-
-
-bot.add_cog(GameCog(bot))
-bot.add_cog(CharCog(bot))
-bot.add_cog(SpendCog(bot))
-bot.add_cog(SessionCog(bot))
-
-bot.run(os.getenv("TOKEN"))  # run the bot with the token
