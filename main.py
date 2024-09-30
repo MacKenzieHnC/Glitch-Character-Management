4bc4c322f24fca50802b10448be9e2bbe4228b0a
MacKenzie H&C <mackenziehnc@gmail.com>
2024-09-30 09:17:03 -0500

    refactor: undo refactor

--- main.py
+++ main.py
@@ -2,13 +2,8 @@ import discord
 import os  # default module
 from dotenv import load_dotenv
 
-
-def boot():
-    load_dotenv()  # load all the variables from the env file
-    bot = discord.Bot()
-    for cog in ["cogs.game", "cogs.costs", "cogs.char", "cogs.session"]:
-        bot.load_extension(cog)
-    bot.run(os.getenv("TOKEN"))  # run the bot with the token
-
-
-boot()
+load_dotenv()  # load all the variables from the env file
+bot = discord.Bot()
+for cog in ["cogs.game", "cogs.costs", "cogs.char", "cogs.session"]:
+    bot.load_extension(cog)
+bot.run(os.getenv("TOKEN"))  # run the bot with the token
