f173e3c6d5fd1725cba5a887e341368b5bb330b3
MacKenzie H&C <mackenziehnc@gmail.com>
2024-09-30 08:25:46 -0500

    refactor: simplify boot

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import discord
 import os  # default module
 from dotenv import load_dotenv
 
-load_dotenv()  # load all the variables from the env file
-bot = discord.Bot()
+
+def boot():
+    load_dotenv()  # load all the variables from the env file
+    bot = discord.Bot()
+    for cog in ["cogs.game", "cogs.costs", "cogs.char", "cogs.session"]:
+        bot.load_extension(cog)
+    bot.run(os.getenv("TOKEN"))  # run the bot with the token
 
 
-bot.load_extension("cogs.game")
-bot.load_extension("cogs.costs")
-bot.load_extension("cogs.char")
-bot.load_extension("cogs.session")
-
-bot.run(os.getenv("TOKEN"))  # run the bot with the token
+boot()
